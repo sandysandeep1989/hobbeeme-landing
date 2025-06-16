e75051581a9ead3513079ba946de1f46585fa009
akash101997 <akash.singh@webninjaz.com>
2025-06-16 15:36:52 +0530

    implemented page.js

--- app/page.js
+++ app/page.js
@@ -1,37 +1,38 @@
-'use client'
 import Image from "next/image";
 import HomePage from './homePage'
 import OfferLanding from "./offer-landing/page";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import styles from './page.module.css'
-import SubscriptionForm from './components/SubscriptionForm'
 
 export default function Home() {
   return (
-    <>
-      {/* <OfferLanding/> */}
-      <Header/>
-      <main className={styles.comingSoonWrapper}>
-        <div>
-          <Image
-            src="/bee.png"
-            alt="Bee Logo"
-            width={300}
-            height={300}
-            className={styles.beeImage}
-          />
-          <h1 className={styles.heading}>
-            <span className={styles.white}>WE ARE</span>{' '}
-            <span className={styles.gray}>COMING SOON</span>
-          </h1>
-          <p className={styles.description}>
-            We're working hard to bring you something amazing. Stay tuned for updates!
-          </p>
-          <SubscriptionForm />
-        </div>
-      </main>
-      <Footer/>
-    </>
+   <>
+    {/* <OfferLanding/> */}
+    <Header/>
+    <div className={styles.comingSoonWrapper}>
+      <div className={styles.comingSoonContainer}>
+      <img src="/beeImage.png" alt="Bee Mascot" className={styles.beeImage} />
+      <h1 className={styles.heading}>
+        <span className={styles.white}>WE ARE</span>{' '}
+        <span className={styles.gray}>COMING SOON</span>
+      </h1>
+      <p className={styles.description}>
+        We are almost there! If you want to get notified when we launch our service in UAE,
+        subscribe to our mailing list!
+      </p>
+
+      <div className={styles.subscriptionBox}>
+        <input
+          type="email"
+          placeholder="Enter your email address"
+          className={styles.input}
+        />
+        <button className={styles.button}>NOTIFY ME →</button>
+      </div>
+      </div>
+    </div>
+    <Footer/>
+   </>
   );
 }
